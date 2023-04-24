4d8cc8002277c9a09fc60ca2be8804066add58e3
Benounnas Oussama <benounnas.oussama@gmail.com>
2023-04-24 11:16:45 +0100

    init and add meta

---
+++ components/IndexComponent.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite()
+})
+
+/* useHead({
+  title: 'My App',
+  meta: [
+    {
+      name: 'description',
+      content: 'My amazing site.'
+    }
+  ]
+}) */
+</script>
+
+<template>
+  <div>
+    <section class="flex items-center justify-center h-screen">
+      <div
+        class="flex flex-col justify-center items-center py-8 px-4 max-w-screen-md text-center lg:py-16 lg:px-12"
+      >
+        <nuxt-picture
+          fit="fill"
+          src="/images/logo.png"
+          sizes="sm:50vw md:50vw lg:200px"
+        />
+        <div>
+          <h1
+            class="mt-3 mb-4 text-4xl font-bold tracking-tight leading-none lg:mb-6 md:text-5xl xl:text-6xl"
+          >
+            Bientôt disponible.
+          </h1>
+          <p class="font-light text-gray-500 md:text-lg xl:text-xl">
+            Salut, nous sommes Atlasmob et nous travaillons actuellement sur
+            notre nouveau site web. Restez à l'écoute pour des mises à jour
+            passionnantes alors que nous donnons vie à notre vision en ligne.
+            Notre équipe est dédiée à la création d'une plateforme conviviale et
+            engageante qui présente nos produits et services de la meilleure
+            façon possible. Nous avons hâte de la partager avec vous bientôt !
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexComponent'
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,26 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'under_construction',
+    title: 'Fabrication de meubles en bois sur mesure - Atlasmob',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'fr',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Atlasmob est un atelier spécialisé dans la fabrication de meubles en bois MDF et en bois mélaminé. Nous créons des meubles sur mesure pour répondre à vos besoins spécifiques.',
+      },
       { name: 'format-detection', content: 'telephone=no' },
+      {
+        name: 'keywords',
+        content:
+          'meubles en bois, meubles sur mesure, fabrication de meubles, bois MDF, bois mélaminé',
+      },
+      { name: 'robots', content: 'noindex, nofollow' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
@@ -32,7 +43,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
